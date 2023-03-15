836fa5262eae3cd7ce765a8b4bc947da7de2eb85
EdenilsonGomes <111291385+EdenilsonGomes@users.noreply.github.com>
2023-03-15 01:04:36 -0300

    Aprendendo sobre condiçoes

--- aula011/ex008.js
+++ aula011/ex008.js
@@ -1,2 +1,6 @@
-var vel = 60.5
-console.log(`A velocidade do seu carro é ${vel}Km/h`)
+var vel = 30
+console.log(`A velocidade do seu carro é de ${vel}Km/H`)
+if (vel > 60) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida, MULTADO!')
+}
+console.log('Diriga sempre usando sinto de segurança')
