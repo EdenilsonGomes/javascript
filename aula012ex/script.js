6bf0a144fc8dd139f8d64267499caec8215131d1
EdenilsonGomes <junior13djd@gmail.com>
2023-03-20 16:59:03 -0300

    exercicio interativo com o horario de quem acessa fazendo a imagem e o texto de horario atualizar automaticamente

--- aula012ex/script.js
+++ aula012ex/script.js
@@ -1,21 +1,21 @@
 
 function carregar(){
+    var horario = document.getElementById('horario')
+    var img = document.getElementById('imagem')
     var agora = new Date()
     var hora = agora.getHours()
 
-    var horario = document.getElementById('horario')
-
-
     horario.innerHTML = `Agora são ${hora} horas.`
 
-
-
-    if(hora < 12){
+    if(hora >= 0 && hora < 12){
         document.getElementsByTagName('body')[0].style.backgroundColor = '#C9B78A'
-    }else if(hora < 18){
+        img.src = 'fotomanha.png'
+
+    }else if(hora >= 12 && hora <= 18){
         document.getElementsByTagName('body')[0].style.backgroundColor = '#A77864'
+        img.src = 'fototarde.png'
     }else{
         document.getElementsByTagName('body')[0].style.backgroundColor = '#4A4B4B'
+        img.src = 'fotonoite.png'
     }
 }
-
