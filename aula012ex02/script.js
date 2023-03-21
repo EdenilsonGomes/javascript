f3c28331ed127a50ad8ad8aa57796fbde9d8da3a
EdenilsonGomes <111291385+EdenilsonGomes@users.noreply.github.com>
2023-03-21 01:05:10 -0300

    realizando o segundo exercicio da aula 012 utlizando javascript para fazer um verificador de idade

---
+++ aula012ex02/script.js
@@ -0,0 +1,12 @@
+function verificar(){
+//verificacao idade
+var data = new Date()
+var ano = data.getFullYear()
+var nasc = document.getElementById('txtn1').value
+var msg = document.getElementById('msg')
+var idade = ano - nasc
+//verificacao sexo
+
+}
+
+
