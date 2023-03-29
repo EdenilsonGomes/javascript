fa4d1157b1a13c55b283f2e9bd9d5c6ef25ebe0c
EdenilsonGomes <junior13djd@gmail.com>
2023-03-29 17:05:27 -0300

    aula016, aula017, aula018

---
+++ profluizotaviomiranda/aula016/index.js
@@ -0,0 +1,37 @@
+//              0       1        2         3       4
+let alunos = ['Luiz', 'Maria', 'João']; //Luiza, Eduardo
+alunos = 123; //Luiza, Eduardo
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array)
+
+//alunos.push('Luiza') //Adiciona no fim
+//alunos.push('Eduardo')
+
+//console.log(alunos.slice(0, -3))// seleciona uma quantidade
+
+// console.log(alunos[50]);
+
+//delete alunos [1];
+//console.log(alunos[1]);;
+
+//const removido = alunos.shift() // remove um array
+//console.log(alunos)
+//console.log(removido)
+
+//alunos.unshift('Luiza') // Adiciona no começo
+//alunos.unshift('Fábio')
+
+//alunos.push('Luiza'); //Adiciona no FIm
+//alunos.push('Fábio');
+
+//alunos[alunos.length] = 'Luiza' //Adiciona no FIm
+//alunos[alunos.length] = 'Fábio'
+//alunos[alunos.length] = 'Luana'
+
+//alunos[0] = 'Eduardo'; //altera
+//alunos[3] = 'Luiza'
+
+// console.log(alunos)
+//console.log(alunos[0])
+//console.log(alunos[2]) 
