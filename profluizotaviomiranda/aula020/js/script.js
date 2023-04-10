8c46336ba957fca087114758d5d87369aa213397
EdenilsonGomes <junior13djd@gmail.com>
2023-04-10 17:04:22 -0300

    Tentando formas de executar o exercicio da aula 20

--- profluizotaviomiranda/aula020/js/script.js
+++ profluizotaviomiranda/aula020/js/script.js
@@ -1,8 +1,32 @@
-function meuEscopo() {
+function meuEscopo (){
    const form = document.querySelector('.form');
+   const resultado = document.querySelector('.resultado')
 
-   form.onsubmit = function (evento){
-    alert(1)
-   };
+   
+
+   function recebeEventoForm (evento) {
+      evento.preventDefault();
+      const nome = form.querySelector('.nome');
+      const sobrenome = form.querySelector('.sobrenome');
+      const peso = form.querySelector('.peso');
+      const altura = form.querySelector('.altura');
+
+   pessoas.push(criaPessoa());
+
+   
+   }
+   
+   function criaPessoa (nome, sobrenome, peso, altura){
+      return {
+         nome: nome,
+         sobrenome: sobrenome,
+         peso: peso,
+         altura: altura
+      };
+      
+      
+   } 
+   const pessoas = [nome, sobrenome, peso, altura];
+   form.addEventListener('submit', recebeEventoForm);
 }
-meuEscopo();
+meuEscopo()
