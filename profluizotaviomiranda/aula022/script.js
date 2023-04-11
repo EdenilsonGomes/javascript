fe8afbec52e1021e6befe6e64409eaff59176582
EdenilsonGomes <junior13djd@gmail.com>
2023-04-11 17:24:36 -0300

    aula 021 && aula 022

---
+++ profluizotaviomiranda/aula022/script.js
@@ -0,0 +1,13 @@
+/* 
+Operadores Lógicos
+&& -> And -> E -> Todas as expressoes precisam ser verdadeiras para retornar (true)
+|| -> Or -> Ou
+! -> not -> não
+*/
+// const expressaoAnd = true && true && false && true
+/* 
+    const usuario = 'Luiz';
+    const senha = '123456'
+    const vaiLogar = usuario === 'Luiz' && senha === '123456'; 
+*/
+console.log(vaiLogar);
