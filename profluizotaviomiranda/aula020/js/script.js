dada995251d0a888c73cf2399882cf552578044d
EdenilsonGomes <junior13djd@gmail.com>
2023-04-11 16:16:00 -0300

    Realização do exercicio da aula 020 realizado

--- profluizotaviomiranda/aula020/js/script.js
+++ profluizotaviomiranda/aula020/js/script.js
@@ -1,32 +1,35 @@
 function meuEscopo (){
-   const form = document.querySelector('.form');
-   const resultado = document.querySelector('.resultado')
+      const form = document.querySelector('.form');
+      const resultado = document.querySelector('.resultado')
 
-   
-
+      const pessoas = [];
+// Isso para o load da pagin com o .preventDefault() e ja recebe o valor que vem do nome, sobrenome, peso, altura
    function recebeEventoForm (evento) {
       evento.preventDefault();
       const nome = form.querySelector('.nome');
       const sobrenome = form.querySelector('.sobrenome');
       const peso = form.querySelector('.peso');
       const altura = form.querySelector('.altura');
-
-   pessoas.push(criaPessoa());
-
+// Isso adiciona ao fim do array -> pessoas[]
+   pessoas.push({
+      nome: nome.value,
+      sobrenome: sobrenome.value,
+      peso: peso.value,
+      altura: altura.value
+   });
+// esse console.log não parece importante, mas é! Ele que vai mostrar no console o Objeto pessoas[]
+    
+   console.log(pessoas); 
    
-   }
+   resultado.innerHTML += `<p> ${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`;
    
-   function criaPessoa (nome, sobrenome, peso, altura){
-      return {
-         nome: nome,
-         sobrenome: sobrenome,
-         peso: peso,
-         altura: altura
-      };
-      
-      
-   } 
-   const pessoas = [nome, sobrenome, peso, altura];
+  } 
+// esse comando seria uma especie de shorthand do evento onSubmit, está dizendo: Quando "Submitar", ira receber "recebeEventoForm"
+  
    form.addEventListener('submit', recebeEventoForm);
+
 }
+// E com isso estou declarando a função para ser exibida
 meuEscopo()
+
+ 
