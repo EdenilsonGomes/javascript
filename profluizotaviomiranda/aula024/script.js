92bdf14e365a4f21f2a1599e33809165102d101d
EdenilsonGomes <junior13djd@gmail.com>
2023-04-13 15:30:48 -0300

    aula024 e aula025

---
+++ profluizotaviomiranda/aula024/script.js
@@ -0,0 +1,32 @@
+/* 
+Entre 0 - 11 - Bom dia
+entr 12 - 17 - Boa tarde
+Entre 18 - 23 - Boa noite
+*/
+
+//if pode ser usado sozinho
+//sempre que utilizo a palabra else, preciso de um if antes
+//Eu possos ter varios else ifs na checagem
+//Só posso te rum else na checagem
+//podemos usar condições sem else if, utilizando apneas if e elese
+
+
+/* const hora = 203;
+
+if( hora >= 0 && hora <= 11) {
+    console.log('Bom dia!')
+}else if(hora >= 12 && hora <= 17){
+    console.log('Boa tarde')
+}else if(hora >= 18 && hora <= 23){
+    console.log('Boa noite')
+}else{
+    console.log('Olá!')
+} */
+
+const tenhoGrana = null;
+
+if (tenhoGrana){
+    console.log('vou sair de casa')
+}else{
+    console.log('Não vou sair de casa')
+}
---
+++ profluizotaviomiranda/aula025/script.js
@@ -0,0 +1,10 @@
+const numero = 10;
+
+//Se (numero >= 0 && numero <= 5) ocorrer, faça isso{codigo}
+//Se não faça isso {o código}
+
+if(numero >= 0 && numero <= 5){
+console.log('O número está entre 0 e 5.')
+}else if (numer){
+
+}
