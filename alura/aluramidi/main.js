fef00f979a05f8973956b89aa595f795513ce69f
EdenilsonGomes <111291385+EdenilsonGomes@users.noreply.github.com>
2023-07-04 09:03:54 -0300

    mexendo com eventos do teclados 'onkeydown' e 'onkeyup' e um pouco de operadores logicos no caso dessa vez foi o de igualdade '===' e o Ou '||'

--- alura/aluramidi/main.js
+++ alura/aluramidi/main.js
@@ -35,7 +35,17 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const idAudio = `#som_${instrumento}`;//template string
 
     tecla.onclick = function () {
-        tocaSom(idAudio)
+        tocaSom(idAudio);
     }
 
+    tecla.onkeydown = function (evento) {
+        
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+
+    }
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa')
+    }
 }
