193de7d8c42753d74a700998fdf1963118dc5544
EdenilsonGomes <111291385+EdenilsonGomes@users.noreply.github.com>
2023-07-18 17:56:32 -0300

    Revendo algumas aulas e mudando o andamento do código.

--- alura/-robotron-2000-projeto_inicial/js/main.js
+++ alura/-robotron-2000-projeto_inicial/js/main.js
@@ -1,21 +1,23 @@
-const subtrair = document.querySelector("#subtrair")
-const somar = document.querySelector("#somar")
-const braco = document.querySelector("#braco")
+const subtrair = document.querySelector('#subtrair')
+const somar = document.querySelector('#somar')
 
-const controle =  document.querySelectorAll('.controle-ajuste')
 
-controle.forEach ( (elemento) => {
-    elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent)
+const controle = document.querySelectorAll('.controle-ajuste')
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) =>{
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        
     })
+
 })
 
-
-
-function manipulaDados(operacao){
+function manipulaDados (operacao, controle){
+    const peca = controle.querySelector('.controle-contador')
+    
     if(operacao === "-"){
-        braco.value = parseInt(braco.value) - 1
+        peca.value = parseInt(peca.value) - 1
     }else {
-        braco.value = parseInt(braco.value) + 1
+        peca.value = parseInt(peca.value) + 1 
     }
 }
